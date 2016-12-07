f7600ead8af634da4d3ddba0b96ff2ee23d6df08
Viacheslav Liashuk <viacheslav.mail@gmail.com>
2016-12-07 13:22:42 +0200

    add 2D cluster model

--- visual_cluster/Program.cs
+++ visual_cluster/Program.cs
@@ -16,7 +16,7 @@ namespace visual_cluster
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new clusterForm());
         }
     }
 }
